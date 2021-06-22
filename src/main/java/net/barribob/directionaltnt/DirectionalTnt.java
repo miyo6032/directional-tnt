fea4d10dccf4cb5c76c03771094e910e9b6eb018
Michael Yoshimura <miyo6032@colorado.edu>
2021-06-22 08:04:12 -0600

    Basic block

--- src/main/java/net/barribob/directionaltnt/DirectionalTnt.java
+++ src/main/java/net/barribob/directionaltnt/DirectionalTnt.java
@@ -1,14 +1,22 @@
 package net.barribob.directionaltnt;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
 public class DirectionalTnt implements ModInitializer {
+	public static final Block DIRECTIONAL_TNT = new Block(FabricBlockSettings.copy(Blocks.TNT));
+
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		System.out.println("Hello Fabric world!");
+		Identifier tntId = new Identifier("directionaltnt", "directional_tnt");
+		Registry.register(Registry.BLOCK, tntId, DIRECTIONAL_TNT);
+		Registry.register(Registry.ITEM, tntId, new BlockItem(DIRECTIONAL_TNT, new FabricItemSettings().group(ItemGroup.MISC)));
 	}
 }
